1844e49311d65493d80c4d60bd162c71abf91dee
web3gaming <99609343+web3gaming@users.noreply.github.com>
2026-03-06 20:27:11 +0500

    Update twitter.js

--- api/twitter.js
+++ api/twitter.js
@@ -1,34 +1,29 @@
 export default async function handler(req, res) {
 
-const sources = [
-"https://cdn.syndication.twimg.com/widgets/followbutton/info.json?screen_names=ALERTX360",
-"https://cdn.syndication.twimg.com/widgets/followbutton/info.json?screen_names=MonitorX99800"
-];
+  try {
 
-try {
+    const accounts = [
+      "https://cdn.syndication.twimg.com/widgets/followbutton/info.json?screen_names=ALERTX360",
+      "https://cdn.syndication.twimg.com/widgets/followbutton/info.json?screen_names=MonitorX99800"
+    ];
 
-const responses = await Promise.all(
-sources.map(url => fetch(url))
-);
+    const responses = await Promise.all(accounts.map(url => fetch(url)));
+    const data = await Promise.all(responses.map(r => r.json()));
 
-const data = await Promise.all(
-responses.map(r => r.json())
-);
+    const users = data.flat();
 
-const accounts = data.flat();
+    res.status(200).json({
+      success: true,
+      users
+    });
 
-res.status(200).json({
-success: true,
-accounts
-});
+  } catch (error) {
 
-} catch (error) {
+    res.status(500).json({
+      success: false,
+      error: "Twitter monitor failed"
+    });
 
-res.status(500).json({
-success: false,
-error: "Failed to fetch twitter accounts"
-});
-
-}
+  }
 
 }
